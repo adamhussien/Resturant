a94de4b550e671f74e0316b09169f89595c83540
adamhussien <114872012+adamhussien@users.noreply.github.com>
2023-02-05 05:59:38 -1200

    first commit

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const menu = document.querySelector('.menu');
+const hamburger = document.querySelector('.hamburger');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+})
